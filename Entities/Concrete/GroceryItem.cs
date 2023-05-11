ac6628e560f2c54cdb3c0e53196fb0dd2e019e7d
Kubilay Kaplan <kkaplan@accodea.onmicrosoft.com>
2023-05-11 09:43:49 +0300

    Entities

---
+++ Entities/Concrete/GroceryItem.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroceryDal
+{
+    public class GroceryItem : IEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+
+        public int Quantity { get; set; } = 1;
+
+    }
+}
