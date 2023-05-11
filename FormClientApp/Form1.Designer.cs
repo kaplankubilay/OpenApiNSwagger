6a584064431ad19ded54f26bddd4bb905c3b5500
Kubilay Kaplan <kkaplan@accodea.onmicrosoft.com>
2023-05-11 13:12:54 +0300

    Added was FormClient

---
+++ FormClientApp/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace FormClientApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groceryDBDataSet = new FormClientApp.GroceryDBDataSet();
+            this.groceryItemBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.groceryItemTableAdapter = new FormClientApp.GroceryDBDataSetTableAdapters.GroceryItemTableAdapter();
+            this.ıdDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.quantityDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.NameTxtBox = new System.Windows.Forms.TextBox();
+            this.NameLbl = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.DescriptionTxtBox = new System.Windows.Forms.TextBox();
+            this.labelControl3 = new DevExpress.XtraEditors.LabelControl();
+            this.QuantityTxtBox = new System.Windows.Forms.TextBox();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_rapor = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groceryDBDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groceryItemBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ıdDataGridViewTextBoxColumn,
+            this.nameDataGridViewTextBoxColumn,
+            this.descriptionDataGridViewTextBoxColumn,
+            this.quantityDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.groceryItemBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(355, 39);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(443, 399);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groceryDBDataSet
+            // 
+            this.groceryDBDataSet.DataSetName = "GroceryDBDataSet";
+            this.groceryDBDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // groceryItemBindingSource
+            // 
+            this.groceryItemBindingSource.DataMember = "GroceryItem";
+            this.groceryItemBindingSource.DataSource = this.groceryDBDataSet;
+            // 
+            // groceryItemTableAdapter
+            // 
+            this.groceryItemTableAdapter.ClearBeforeFill = true;
+            // 
+            // ıdDataGridViewTextBoxColumn
+            // 
+            this.ıdDataGridViewTextBoxColumn.DataPropertyName = "Id";
+            this.ıdDataGridViewTextBoxColumn.HeaderText = "Id";
+            this.ıdDataGridViewTextBoxColumn.Name = "ıdDataGridViewTextBoxColumn";
+            this.ıdDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "Name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Name";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            // 
+            // descriptionDataGridViewTextBoxColumn
+            // 
+            this.descriptionDataGridViewTextBoxColumn.DataPropertyName = "Description";
+            this.descriptionDataGridViewTextBoxColumn.HeaderText = "Description";
+            this.descriptionDataGridViewTextBoxColumn.Name = "descriptionDataGridViewTextBoxColumn";
+            // 
+            // quantityDataGridViewTextBoxColumn
+            // 
+            this.quantityDataGridViewTextBoxColumn.DataPropertyName = "Quantity";
+            this.quantityDataGridViewTextBoxColumn.HeaderText = "Quantity";
+            this.quantityDataGridViewTextBoxColumn.Name = "quantityDataGridViewTextBoxColumn";
+            // 
+            // NameTxtBox
+            // 
+            this.NameTxtBox.Location = new System.Drawing.Point(95, 39);
+            this.NameTxtBox.Name = "NameTxtBox";
+            this.NameTxtBox.Size = new System.Drawing.Size(95, 20);
+            this.NameTxtBox.TabIndex = 1;
+            // 
+            // NameLbl
+            // 
+            this.NameLbl.Location = new System.Drawing.Point(21, 42);
+            this.NameLbl.Name = "NameLbl";
+            this.NameLbl.Size = new System.Drawing.Size(27, 13);
+            this.NameLbl.TabIndex = 2;
+            this.NameLbl.Text = "Name";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(21, 73);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(53, 13);
+            this.labelControl2.TabIndex = 4;
+            this.labelControl2.Text = "Description";
+            // 
+            // DescriptionTxtBox
+            // 
+            this.DescriptionTxtBox.Location = new System.Drawing.Point(95, 70);
+            this.DescriptionTxtBox.Name = "DescriptionTxtBox";
+            this.DescriptionTxtBox.Size = new System.Drawing.Size(95, 20);
+            this.DescriptionTxtBox.TabIndex = 3;
+            // 
+            // labelControl3
+            // 
+            this.labelControl3.Location = new System.Drawing.Point(21, 106);
+            this.labelControl3.Name = "labelControl3";
+            this.labelControl3.Size = new System.Drawing.Size(42, 13);
+            this.labelControl3.TabIndex = 6;
+            this.labelControl3.Text = "Quantity";
+            // 
+            // QuantityTxtBox
+            // 
+            this.QuantityTxtBox.Location = new System.Drawing.Point(95, 103);
+            this.QuantityTxtBox.Name = "QuantityTxtBox";
+            this.QuantityTxtBox.Size = new System.Drawing.Size(95, 20);
+            this.QuantityTxtBox.TabIndex = 5;
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(95, 143);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(75, 23);
+            this.btn_save.TabIndex = 7;
+            this.btn_save.Text = "Kaydet";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_rapor
+            // 
+            this.btn_rapor.Location = new System.Drawing.Point(355, 10);
+            this.btn_rapor.Name = "btn_rapor";
+            this.btn_rapor.Size = new System.Drawing.Size(75, 23);
+            this.btn_rapor.TabIndex = 8;
+            this.btn_rapor.Text = "Rapor";
+            this.btn_rapor.UseVisualStyleBackColor = true;
+            this.btn_rapor.Click += new System.EventHandler(this.btn_rapor_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btn_rapor);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.labelControl3);
+            this.Controls.Add(this.QuantityTxtBox);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.DescriptionTxtBox);
+            this.Controls.Add(this.NameLbl);
+            this.Controls.Add(this.NameTxtBox);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groceryDBDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.groceryItemBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private GroceryDBDataSet groceryDBDataSet;
+        private System.Windows.Forms.BindingSource groceryItemBindingSource;
+        private GroceryDBDataSetTableAdapters.GroceryItemTableAdapter groceryItemTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ıdDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn descriptionDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn quantityDataGridViewTextBoxColumn;
+        private System.Windows.Forms.TextBox NameTxtBox;
+        private DevExpress.XtraEditors.LabelControl NameLbl;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private System.Windows.Forms.TextBox DescriptionTxtBox;
+        private DevExpress.XtraEditors.LabelControl labelControl3;
+        private System.Windows.Forms.TextBox QuantityTxtBox;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_rapor;
+    }
+}
+
---
+++ FormClientApp/Form1.cs
@@ -0,0 +1,67 @@
+﻿using NSwagGrocerySdk;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace FormClientApp
+{
+    public partial class Form1 : Form
+    {
+        private GroceryClient _groceryClient;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        //TODO: This line of code loads data into the 'groceryDBDataSet.GroceryItem' table.You can move, or remove it, as needed.
+        //    this.groceryItemTableAdapter.Fill(this.groceryDBDataSet.GroceryItem);
+        }
+
+        private void btn_rapor_Click(object sender, EventArgs e)
+        {
+            GetGroceryItemList();
+        }
+
+        private async void btn_save_Click(object sender, EventArgs e)
+        {
+            var getGroceryClient = CreateClient();
+            await getGroceryClient.AddGroceryItemAsync(new GroceryItem
+            {
+                Name = NameTxtBox.Text,
+                Description = DescriptionTxtBox.Text,
+                Quantity = Convert.ToInt32(QuantityTxtBox.Text)
+            });
+
+            GetGroceryItemList();
+        }
+
+        private GroceryClient CreateClient()
+        {
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1bmlxdWVfbmFtZSI6ImtrYXBsYW4iLCJzdWIiOiJra2FwbGFuIiwianRpIjoiOGI0N2VjZiIsImF1ZCI6WyJodHRwOi8vbG9jYWxob3N0OjYxNjEiLCJodHRwczovL2xvY2FsaG9zdDo0NDM4OCIsImh0dHA6Ly9sb2NhbGhvc3Q6NTA1NiIsImh0dHBzOi8vbG9jYWxob3N0OjcxMTciXSwibmJmIjoxNjgzNzI2MTgwLCJleHAiOjE2OTE2NzQ5ODAsImlhdCI6MTY4MzcyNjE4MCwiaXNzIjoiZG90bmV0LXVzZXItand0cyJ9.g7RvVs5MtlSKux674lpxu91cJL-uP2zp58T6ePDsUBM");
+
+            var groceryClient = new GroceryClient("https://localhost:7117", client);
+
+            return groceryClient;
+        }
+
+        private async void GetGroceryItemList()
+        {
+            var getGroceryClient = CreateClient();
+            var items = await getGroceryClient.GetGroceryItemsAsync();
+            var asd = (List<GroceryItem>)items.Data;
+            groceryItemBindingSource.DataSource = asd;
+        }
+    }
+}
